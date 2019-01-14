65b1eca199286f06fd762b5b47cafa71d7611610
janainfanger <43797937+janainfanger@users.noreply.github.com>
2019-01-14 12:06:48 -0800

    This is after the database is migrated A2
    Add a database called "pythonclub" to PostgreSQL. Configure the Django settings to add "club" to the app list and to make PostgreSQL with python club your default database. Edit the URL in the pythonclub folder to import the URLS from the club folder. Add a urls.py file to the club folder. Migrate the database.

--- pythonclub/pythonclub/settings.py
+++ pythonclub/pythonclub/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'club',
 ]
 
 MIDDLEWARE = [
@@ -74,11 +75,16 @@ WSGI_APPLICATION = 'pythonclub.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': 'pythonclubdb',
+       'USER' : 'postgres',
+       'PASSWORD' : 'P@ssw0rd1',
+       'HOST' : 'localhost',
+       'PORT' : '',
+   }
 }
+
 
 
 # Password validation
