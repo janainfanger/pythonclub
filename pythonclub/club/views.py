96c2f872aa8ae5ce2fc6c5c370b3db457945d793
janainfanger <43797937+janainfanger@users.noreply.github.com>
2019-01-30 11:15:46 -0800

    Adds resource list page
    Use the admin to add some users
    
    Create a group called members, add a couple of the users to it
    
    Add two or three resources
    
    Add a view that returns all the resources
    
    Create a template for the view
    
    Add the url for the view
    
    Add a link to the view in the base.html

--- pythonclub/club/views.py
+++ pythonclub/club/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from.models import Resource
 
 # Create your views here.
 
 def index(request):
     return render(request, 'club/index.html')
+
+def resources(request):
+    resources=Resource.objects.all()
+    return render(request, 'club/resources.html', {'resources': resources})
