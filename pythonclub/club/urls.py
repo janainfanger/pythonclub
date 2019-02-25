5a1265b98aecbfaf3905af075b2407aeaf2cdb67
janainfanger <43797937+janainfanger@users.noreply.github.com>
2019-02-25 12:21:11 -0800

    Adds Meeting page and details page
    Add a view that returns all the meetings:
    
    Make a template that displays all the meeting items in a list
    
    modify url.py to contain this view. 
    
    Add a link to the base.html to add meetings to the menu
    
    Try the link. Troubleshoot until it works
    
    Make a view that takes an int id as an parameter and retrieves the meeting that has that id
    
    modify the meeting template so that the title is a link to a detail page and each link returns the id
    
    Create the template for the detail
    
    modify the url to include the id
    
    Try the link and troubleshoot until it works
    
    Upload the code to GitHub and post the link for the assignment

--- pythonclub/club/urls.py
+++ pythonclub/club/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
 path('', views.index, name='index' ),
-path('resources/', views.resources, name='resources')
-
+path('resources/', views.resources, name='resources'),
+path('getmeetings/', views.getmeetings, name='getmeetings'),
+path('meetingdetail/<int:id>', views.meetingdetail, name='details') #the int id part includes the id in the url
 ]
--- pythonclub/club/views.py
+++ pythonclub/club/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from.models import Resource
+from django.shortcuts import render, get_object_or_404
+from.models import Resource, Meeting
 
 # Create your views here.
 
@@ -9,3 +9,14 @@ def index(request):
 def resources(request):
     resources=Resource.objects.all()
     return render(request, 'club/resources.html', {'resources': resources})
+
+def getmeetings(request):
+    meeting_list=Meeting.objects.all()
+    return render(request, 'club/meetings.html', {'meeting_list': meeting_list})
+
+def meetingdetail(request, id):
+    detail=get_object_or_404(Meeting, pk=id)
+    context = { 'detail': detail}
+    return render (request, 'club/details.html', context=context)
+
+
